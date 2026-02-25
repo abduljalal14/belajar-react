9132c5af3099ce2b7b359fc3e6e7dcdc78cd8e8a
abduljalal14 <abdoeljalalexotic@gmail.com>
2026-02-25 15:06:06 +0700

    feat(Route): add Post api route

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -2,28 +2,48 @@
 
 namespace App\Http\Controllers\Api;
 
-//import Model "Post"
-use App\Models\Post;
-
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-
-//import Resource "PostResource"
-use App\Http\Resources\PostResource;
+use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     /**
-     * index
-     *
-     * @return void
+     * Display a listing of the resource.
      */
     public function index()
     {
-        //get all posts
-        $posts = Post::latest()->paginate(5);
+        //
+    }
 
-        //return collection of posts as a resource
-        return new PostResource(true, 'List Data Posts', $posts);
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
     }
 }
