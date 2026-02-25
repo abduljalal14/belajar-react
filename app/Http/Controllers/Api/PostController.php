f0e7edd586cb5111802a040bd9d837e0851f7fcd
abduljalal14 <abdoeljalalexotic@gmail.com>
2026-02-25 15:11:28 +0700

    fix(Controller) fix post controller

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -2,48 +2,28 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
+//import Model "Post"
+use App\Models\Post;
+
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+//import Resource "PostResource"
+use App\Http\Resources\PostResource;
 
 class PostController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * index
+     *
+     * @return void
      */
     public function index()
     {
-        //
-    }
+        //get all posts
+        $posts = Post::latest()->paginate(5);
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
+        //return collection of posts as a resource
+        return new PostResource(true, 'List Data Posts', $posts);
     }
 }
